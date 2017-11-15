9c214d453c2ebd70bb4c0bd53462b3feae8c6286
Andrey Voyvodov <a.voivodov@gmail.com>
2017-11-15 14:02:14 +0200

    Update Assert.AreEqual on function Register()

--- Tests.cs
+++ Tests.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Linq;
 using System.Threading;
 
 namespace SeleniumDemo
@@ -14,17 +15,25 @@ namespace SeleniumDemo
         public void OpenBrowser()
         {
             this.Driver = new ChromeDriver();
+
+            if (Driver.Manage().Cookies.AllCookies.Any())
+            {
+                Driver.Manage().Cookies.DeleteAllCookies(); 
+            }
         }
         [TestMethod]
         public void Register()
         {
             this.Driver.Navigate().GoToUrl("http://store.demoqa.com/wp-login.php?action=register");
-            Thread.Sleep(6000);
+            
             Driver.FindElement(By.Id("user_login")).SendKeys(Faker.Internet.UserName());
 
             Driver.FindElement(By.Id("user_email")).SendKeys(Faker.Internet.Email());
 
-            Driver.FindElement(By.Id("wp-submit")).Click();
+            Driver.FindElement(By.Id("wp-submit")).Click(); 
+
+            Assert.AreEqual("Registration complete. Please check your email.", Driver.FindElement(By.CssSelector(".message")).Text);
+
         }
 
         [TestMethod]
@@ -32,15 +41,16 @@ namespace SeleniumDemo
         {
             this.Driver.Navigate().GoToUrl("http://store.demoqa.com/products-page/product-category/");
             Thread.Sleep(3001);
-            Driver.FindElement(By.CssSelector("div.rater-0:nth-child(3) > a:nth-child(1)")).Click();
-            Driver.FindElement(By.CssSelector("div.rater-1:nth-child(5) > a:nth-child(4)")).Click();
-            Driver.FindElement(By.CssSelector("div.rater-2:nth-child(4) > a:nth-child(2)")).Click(); 
+            Driver.FindElement(By.CssSelector("div.rater-0:nth-child(4) > a:nth-child(1)")).Click();
+            Driver.FindElement(By.CssSelector("div.rater-1:nth-child(5) > a:nth-child(1)")).Click();
+            Driver.FindElement(By.CssSelector("div.rater-2:nth-child(4) > a:nth-child(1)")).Click(); 
+           
 
         }
         [TestCleanup]
         public void CloseBrowser()
         {
-            this.CloseBrowser();
+            Driver.Close();
         }
     }
 }
