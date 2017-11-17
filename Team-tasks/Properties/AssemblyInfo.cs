9053321e5e2317419b8d312c0ad30a856b231f50
Andrey Voyvodov <a.voivodov@gmail.com>
2017-11-17 20:06:59 +0200

    Solution to test case

---
+++ Team-tasks/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Team-tasks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Team-tasks")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b6c4ac4-8d13-48af-944f-928a4aecf3cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Team-tasks/Tests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+using System.Threading;
+
+
+namespace Team_tasks
+{
+    [TestClass]
+    public class Tests
+    {
+        private IWebDriver Driver;
+
+        [TestInitialize]
+        public void OpenBrowser()
+        {
+            this.Driver = new ChromeDriver();
+
+            if (Driver.Manage().Cookies.AllCookies.Any())
+            {
+                Driver.Manage().Cookies.DeleteAllCookies();
+            }
+        }
+
+        [TestMethod]
+        public void Register()
+        {
+            this.Driver.Navigate().GoToUrl("http://store.demoqa.com/wp-login.php?action=register");
+
+            Driver.FindElement(By.Id("user_login")).SendKeys(Faker.Internet.UserName());
+
+            Driver.FindElement(By.Id("user_email")).SendKeys(Faker.Internet.Email());
+
+            Driver.FindElement(By.Id("wp-submit")).Click();
+
+            Assert.AreEqual("Registration complete. Please check your email.", Driver.FindElement(By.CssSelector(".message")).Text);
+
+        }
+
+        [TestMethod]
+        public void Login()
+        {
+
+            this.Driver.Navigate().GoToUrl("http://store.demoqa.com/wp-login.php");
+            Driver.FindElement(By.Id("user_login")).SendKeys("SBonova");
+            Driver.FindElement(By.Id("user_pass")).SendKeys("5WLwf*t7JFBspE4U");
+            Driver.FindElement(By.Id("wp-submit")).Click();
+            Assert.AreEqual("SBonova", Driver.FindElement(By.ClassName("display-name")).Text);
+        }
+
+        [TestMethod]
+        public void Rate()
+        {
+
+            this.Driver.Navigate().GoToUrl("http://store.demoqa.com/products-page/product-category/");
+            Driver.FindElement(By.CssSelector("div.rater-0:nth-child(4) > a:nth-child(1)")).Click();
+            Thread.Sleep(3001);
+            Driver.FindElement(By.CssSelector("div.rater-1:nth-child(5) > a:nth-child(1)")).Click();
+            Thread.Sleep(3001);
+            Driver.FindElement(By.CssSelector("div.rater-2:nth-child(4) > a:nth-child(1)")).Click();
+
+
+        }
+
+        [TestCleanup]
+        public void CloseBrowser()
+        {
+            Driver.Close();
+        }
+    }
+}
